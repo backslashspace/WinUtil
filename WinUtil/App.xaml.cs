f77f8fdb1556bd667a282ed44c15233129ee91f7
backslashspace <86926768+backslashspace@users.noreply.github.com>
2023-03-07 15:49:00 +0100

    Add files via upload

---
+++ WinUtil/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace WinUtil
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
